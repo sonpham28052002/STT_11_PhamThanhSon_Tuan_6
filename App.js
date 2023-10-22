3724e299cebdadc656c1e5d14278fb88f5c54e84
Phạm Thanh Sơn <84237256+sonpham28052002@users.noreply.github.com>
2023-10-22 12:12:24 +0700

    update project

--- App.js
+++ App.js
@@ -1,4 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import four_a from './screen/4_a'
-export default four_a;
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import four_a from "./screen/4_a";
+import four_b from "./screen/4_b";
+import { FontAwesome ,AntDesign} from "@expo/vector-icons";
+
+var Tab = createBottomTabNavigator();
+export default function app() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle:{
+            backgroundColor:"#1BA9FF"
+          }
+        }}>
+        <Tab.Screen
+          name="four_b"
+          component={four_b}
+          options={{
+            title:"",
+            headerStyle: {
+              backgroundColor: "#1BA9FF",
+            },
+            headerTitle: () => {
+              return (
+                <View style={{height:50, width:300,justifyContent:"center"}}>
+                  <TextInput
+                    style={{
+                      height: 40,
+                      width: 250,
+                      backgroundColor: "white",
+                      paddingLeft: 50,
+                    }}
+                    placeholder="hvjkasd"
+                  />
+                  <FontAwesome name="search" size={30} color="black"
+                  style={{position:"absolute", left:10}} />
+                </View>
+              );
+            },
+            headerRight:
+              () => {
+                return(
+                  <View style={{justifyContent:"center"}}>
+                    <FontAwesome name="shopping-cart" size={35} color="white" style={{position:"absolute", right:100, }}/>
+                    <AntDesign name="ellipsis1" size={35} color="white" style={{position:"absolute", right:10, fontWeight:"bold"}}/>
+                    <View style={{backgroundColor:"red", height:15, width:15 , borderRadius:50, position:"absolute", right:95, top:-15}}></View>
+                  </View>
+                );
+              },
+              tabBarIcon:(focus)=>{
+                return(
+                  <FontAwesome name="home" size={40} style={{color:"black"}}/>
+                )
+              }
+          }}
+        />
+        <Tab.Screen
+          name="4_a"
+          component={four_a}
+          headerStyle={{ backgroundColor: "red" }}
+          options={{
+            tabBarIcon:(focus)=>{
+                return(
+                  <FontAwesome name="bars" size={24} style={{color:"black"}} />
+                )
+              },
+              tabBarActiveTintColor:"red",
+            title: "",
+            headerRight: () => (
+              <AntDesign
+                name="shoppingcart"
+                size={35}
+                color="white"
+                style={{ margin: 20 }}
+              />
+            ),
+            headerTitle:() => {
+              return(
+                <Text style={{fontWeight:"bold", fontSize:30}}>Chat</Text>
+              );
+            },
+            headerStyle: {
+              backgroundColor: "#1BA9FF",
+            },
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontSize: 30,
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- screen/4_b.js
+++ screen/4_b.js
@@ -1,6 +1,133 @@
-import React from 'react'
-import {} from 'react-native'
+import React from "react";
+import {View,Text,Image,FlatList,ScrollView,StyleSheet} from "react-native";
+import {FontAwesome} from '@expo/vector-icons'
 
-export default function Four_B(){
-    
+export default function Four_B() {
+  var DATA = [
+    {
+      id: 1,
+      image: "usb",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },
+    {
+      id: 2,
+      image: "dayTainghe",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },
+    {
+      id: 3,
+      image: "usb1",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },
+    {
+      id: 4,
+      image: "usb2",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },
+    {
+      id: 5,
+      image: "usb3",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },
+    {
+      id: 6,
+      image: "usb4",
+      name: "Cáp chuyển từ Cổng USB sang PS2...",
+      star: 4,
+      danhgia: 15,
+      price: "69.900 đ",
+      counpon: "-39%",
+    },{
+        id: 4,
+        image: "usb4",
+        name: "Cáp chuyển từ Cổng USB sang PS2...",
+        star: 4,
+        danhgia: 15,
+        price: "69.900 đ",
+        counpon: "-39%",
+      },
+      {
+        id: 5,
+        image: "usb",
+        name: "Cáp chuyển từ Cổng USB sang PS2...",
+        star: 4,
+        danhgia: 15,
+        price: "69.900 đ",
+        counpon: "-39%",
+      },
+      {
+        id: 6,
+        image: "usb",
+        name: "Cáp chuyển từ Cổng USB sang PS2...",
+        star: 4,
+        danhgia: 15,
+        price: "69.900 đ",
+        counpon: "-39%",
+      },
+  ];
+
+  return(
+    <View style={{flex:1}}>
+        <ScrollView >
+            <FlatList
+                data={DATA}
+                keyExtractor={(item) => item.id}
+                numColumns={2}
+                renderItem={({item})=>{
+                    return(
+                        <View style={styles.itemView}>
+                            <Image
+                                style={{height:"50%", width:"100%"}}
+                                source={require(`../img/${item.image}.jpg`)}
+                            />
+                            <Text style={{fontWeight:"bold", fontSize:15}}>{item.name}</Text>
+                            <View style={{flexDirection:"row"}}>
+                                <View style={{flexDirection:"row"}}>
+                                <FontAwesome name="star" size={20} color="yellow" style={{marginRight:5}}/>
+                                <FontAwesome name="star" size={20} color="yellow" style={{marginRight:5}}/>
+                                <FontAwesome name="star" size={20} color="yellow" style={{marginRight:5}}/>
+                                <FontAwesome name="star" size={20} color="yellow" style={{marginRight:5}}/>
+                                <FontAwesome name="star" size={20} color="gray" style={{marginRight:5}}/>
+                                </View>
+                                <Text style={{color:"gray", marginLeft:10}}>({item.danhgia})</Text>
+                            </View>
+                            <View style={{flexDirection:"row", alignItems:"flex-end"}}>
+                                <Text style={{fontWeight:"bold", fontSize:20, marginRight:20}}>{item.price}</Text>
+                                <Text>{item.counpon}</Text>
+                            </View>
+                        </View>
+                    );                
+                }}
+            />
+        </ScrollView>
+    </View>
+  );
 }
+var styles = StyleSheet.create({
+    itemView:{
+        height:200,
+        width:200,
+        marginLeft:10,
+        marginTop:10
+    }
+})
